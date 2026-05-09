cec72d103856be7700cf6ccbfac0fcdb3b1267e4
iRahmanG <rahmanmr039@gmail.com>
2026-05-09 21:32:16 +0530

    Add CartItem model

--- app/src/main/java/com/example/dailycart/data/local/CartItem.kt
+++ app/src/main/java/com/example/dailycart/data/local/CartItem.kt
@@ -1,8 +1,15 @@
 package com.example.dailycart.data.local
 
+import androidx.room.PrimaryKey
 import androidx.room3.Entity
 
 @Entity(tableName = "cart_table")
 data class CartItem(
-
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val productId: Int,
+    val productName: String,
+    val productPrice: Double,
+    val quantity: Int,
+    val totalPrice: Double
 )
