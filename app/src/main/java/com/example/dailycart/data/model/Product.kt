48c08563410dd0142c4a5670e5363d5583d9d745
iRahmanG <rahmanmr039@gmail.com>
2026-05-09 20:59:47 +0530

    Add Product data class

---
+++ app/src/main/java/com/example/dailycart/data/model/Product.kt
@@ -0,0 +1,10 @@
+package com.example.dailycart.data.model
+
+data class Product(
+    val id: Int,
+    val name: String,
+    val price: Double,
+    val description: String,
+    val category: String,
+    val unit: String
+)
